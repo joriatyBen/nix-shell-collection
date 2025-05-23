791e3000f22ef20a8dad209a55b56dfc2ff83166
joriatyBen <paul.bajorat@gmail.com>
2025-05-23 13:47:35 +0200

    add py flake

--- python-venv/flake2.nix
+++ python-venv/flake2.nix
@@ -1,28 +1,40 @@
  {
-  description = "python venv for anthropic mcp";
+  description = "python venv and tools for BC25";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";    
+    nixpkgs-predecessor.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-predecessor, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system: let
+        pname = "bc_25";
         pkgs = nixpkgs.legacyPackages."${system}";
+        pkgs-predecessor = nixpkgs-predecessor.legacyPackages."${system}";
         venvDir = ".venv";
       in
         rec {
+          inherit pname;
+
           devShell = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
               python312Packages.python
               python312Packages.python-lsp-server
               python312Packages.autopep8
-              uv
-              nodejs_20
+              poetry
+              uv # better faster pip
+              k9s
+              kubectl
+              krew
+              kubelogin
+              pkgs-predecessor.abctl
             ];
-
+            
             shellHook = ''
                 export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH";
+                export PATH="$HOME/.krew/bin:$PATH" 
 
                 if [ -d "${venvDir}" ]; then
                   echo "Skipping venv creation, '${venvDir}' already exists"
@@ -39,4 +51,5 @@
         }
     );
 } 
+
 
